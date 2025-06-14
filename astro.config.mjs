aa691594f5558db7cbd8ac821142f683c97e4bdd
Iván Risueño <ivan.risueno@outlook.com>
2025-06-14 17:23:16 +0200

    Add deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()]
+  ,site: 'https://ivan-risueno.github.io'
+  ,base: '/'
 });
